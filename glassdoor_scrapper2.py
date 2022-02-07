86c89fa7729301f44bcf6e4e372ce598debabba1
Aakanksha3010 <69424461+Aakanksha3010@users.noreply.github.com>
2022-02-07 16:32:55 +0530

    made changes to glassdoor_scrapper2

--- glassdoor_scrapper2.py
+++ glassdoor_scrapper2.py
@@ -4,4 +4,4 @@ import pandas as pd
 path = "D:/Work Space/Projects/Data Science/Glassdoor Project/chromedriver.exe"
 
 df = gs.get_jobs("data scientist",15,False,path,15)
-df
+df.to_csv("glassdoor_jobs.csv",index=False)
